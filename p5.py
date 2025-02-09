25c1bf5cc03e9689d5d0676941318bfe1d2f82a1
DemonDimon <DonecD1m676@gmail.com>
2025-02-09 09:12:43 +0100

    para5

---
+++ p5.py
@@ -0,0 +1,20 @@
+import requests
+
+def first_function():
+    pass
+
+
+class Human:
+    pass
+
+req = requests
+first_f = first_function
+demasik = Human
+
+print(requests.__name__)
+print(req.__name__)
+print(first_function.__name__)
+print(first_f.__name__)
+print(Human.__name__)
+print(demasik.__name__)
+print(ncc.__name__)
