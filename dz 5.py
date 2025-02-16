fa501344c73bd4cb9b5dfaaf8f4281514a136099
DemonDimon <DonecD1m676@gmail.com>
2025-02-16 21:14:12 +0100

    dz 5

---
+++ dz 5.py
@@ -0,0 +1,9 @@
+
+from colorama import Fore, Back, Style, init
+
+init()
+
+print(Fore.RED + "RED")
+print(Back.GREEN + "RED AND GREEN")
+print(Style.BRIGHT + "BIG LETTERS")
+print(Style.RESET_ALL + "NORMAL")
